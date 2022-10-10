5e68d4d5abd53e528582a4cc256ed703f907e2fb
Clayton Salvador <43305715+claytonrh@users.noreply.github.com>
2022-10-10 16:00:09 -0300

    Update Aluno.java

--- src/modelo/heranca/Aluno.java
+++ src/modelo/heranca/Aluno.java
@@ -8,12 +8,13 @@ import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 
+
+// @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS) // para cada classe concreta ter√° uma tabela
+// @Inheritance(strategy = InheritanceType.SINGLE_TABLE) // ir√° criar apenas uma tabela discriminando em uma coluna o tipo do objeto  
 @Entity
-// @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS) // para cada classe concreta ter· uma tabela
-// @Inheritance(strategy = InheritanceType.SINGLE_TABLE) // ir· criar apenas uma tabela discriminando em uma coluna o tipo do objeto  
-@Inheritance(strategy = InheritanceType.JOINED) //ir· criar tambÈm uma tabela para cada classe mas os dados n„o ficaram duplicados, uma das tabelas receber· um PK que se tornar· uma FK
-@DiscriminatorColumn(name = "tipo", length = 2, discriminatorType = DiscriminatorType.STRING) // È obrigatÛrio esta anotaÁ„o para a estratÈgia SingleTable
-@DiscriminatorValue("AL") // esse ser· o "tipo" dessa classe de aluno "AL = Aluno Normal"
+@Inheritance(strategy = InheritanceType.JOINED) //ir√° criar tamb√©m uma tabela para cada classe mas os dados n√£o ficaram duplicados, uma das tabelas receber√° um PK que se tornar√° uma FK
+@DiscriminatorColumn(name = "tipo", length = 2, discriminatorType = DiscriminatorType.STRING) // √© obrigat√≥rio esta anota√ß√£o para a estrat√©gia SingleTable
+@DiscriminatorValue("AL") // esse ser√° o "tipo" dessa classe de aluno "AL = Aluno Normal"
 public class Aluno {
 
 	@Id
